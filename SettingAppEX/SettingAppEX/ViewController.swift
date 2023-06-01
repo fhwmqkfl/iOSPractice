233a029da7a63ee12640c6859953b9561c59cdcb
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-06-02 00:59:42 +0900

    feat(SettingAppEX) : change array for add image

--- SettingAppEX/SettingAppEX/ViewController.swift
+++ SettingAppEX/SettingAppEX/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var mainTableView: UITableView!
     
-    let firstValue: [String] = ["에어플레인 모드", "Wi-Fi", "Bluetooth", "셀룰러", "개인용 핫스팟"]
-    let secondValue: [String] = ["알림", "사운드 및 햅틱", "집중 모드", "스크린 타임"]
-    let thirdValue: [String] = ["일반", "제어센터", "디스플레이 및 밝기", "홈 화면", "손쉬운 사용", "배경화면", "Siri 및 검색", "Face ID 및 암호", "긴급 구조 요청", "노출 알림", "배터리", "개인정보 보호 및 보안"]
-    let fourthValue: [String] = ["App Store", "지갑 및 Apple Pay"]
-    let fifthValue: [String] = ["암호", "연락처", "캘린더", "메모", "미리 알림", "전화", "메세지", "FaceTime"]
-    var settingList: [[String]] = []
+    let firstValue: [(String, UIImage?)] = [("에어플레인 모드", UIImage(systemName: "airplane")), ("Wi-Fi", UIImage(systemName: "wifi")), ("Bluetooth",UIImage(systemName: "bluetooth")), ("셀룰러", UIImage(systemName: "cellularbars")), ("개인용 핫스팟", UIImage(systemName: "personalhotspot"))]
+    let secondValue: [(String, UIImage?)] = [("알림", UIImage(systemName: "bell")), ("사운드 및 햅틱", UIImage(systemName: "bell")), ("집중 모드", UIImage(systemName: "bell")), ("스크린 타임", UIImage(systemName: "bell"))]
+    let thirdValue: [(String, UIImage?)] = [("일반", UIImage(systemName: "bell")), ("제어센터", UIImage(systemName: "bell")), ("디스플레이 및 밝기", UIImage(systemName: "bell")), ("홈 화면", UIImage(systemName: "bell")), ("손쉬운 사용", UIImage(systemName: "bell")), ("배경화면", UIImage(systemName: "bell")), ("Siri 및 검색", UIImage(systemName: "bell")), ("Face ID 및 암호", UIImage(systemName: "bell")), ("긴급 구조 요청" , UIImage(systemName: "bell")), ("노출 알림", UIImage(systemName: "bell")), ("배터리", UIImage(systemName: "bell")), ("개인정보 보호 및 보안", UIImage(systemName: "bell"))]
+    let fourthValue: [(String, UIImage?)] = [("App Store", UIImage(systemName: "bluetooth")), ("지갑 및 Apple Pay", UIImage(systemName: "bluetooth"))]
+    let fifthValue: [(String, UIImage?)] = [("암호", UIImage(systemName: "bluetooth")), ("연락처", UIImage(systemName: "bluetooth")), ("캘린더", UIImage(systemName: "bluetooth")), ("메모", UIImage(systemName: "bluetooth")), ("미리 알림", UIImage(systemName: "bluetooth")), ("전화", UIImage(systemName: "bluetooth")), ("메세지", UIImage(systemName: "bluetooth")), ("FaceTime", UIImage(systemName: "bluetooth"))]
+    var settingList: [[(title: String, image: UIImage?)]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,7 +42,9 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell") as? SettingTableViewCell else { return UITableViewCell() }
-        cell.settingLabel.text = settingList[indexPath.section][indexPath.row]
+        let data = settingList[indexPath.section][indexPath.row]
+        cell.settingLabel.text = data.title
+        cell.settingImageView.image = data.image
         return cell
     }
     
