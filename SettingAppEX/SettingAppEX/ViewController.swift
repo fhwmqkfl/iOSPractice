546680ba4ae75d135f29a52db010bfc33f97deb2
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-06-05 13:06:38 +0900

    Refactor(SettingAppEX) : conform convention

--- SettingAppEX/SettingAppEX/ViewController.swift
+++ SettingAppEX/SettingAppEX/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var mainTableView: UITableView!
     
-    
     var settingList: [Setting] = []
     
     override func viewDidLoad() {
