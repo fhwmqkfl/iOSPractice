bdf888b3deec8f866e9e6e3d08925abd9fa794b1
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-07-04 17:08:26 +0900

    Feat(RealmEX): delete object using user defaults

--- RealmEX/RealmEX/ViewController.swift
+++ RealmEX/RealmEX/ViewController.swift
@@ -14,7 +14,6 @@ enum Gender: String, PersistableEnum {
     case none
 }
 
-
 class Person: Object {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var name: String
@@ -31,7 +30,7 @@ class Person: Object {
 
 class ViewController: UIViewController {
     @IBOutlet weak var numberingLabel: UILabel!
-
+    
     let realm = try! Realm()
     let udid = "udNumbering"
     var numbering: Int = 0
@@ -40,28 +39,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         /// relam local file url
 //        print("file url: \(realm.configuration.fileURL)")
+        let personList = realm.objects(Person.self)
         if UserDefaults.standard.integer(forKey: udid) == 0 {
             UserDefaults.standard.setValue(0, forKey: udid)
         }
         numbering = UserDefaults.standard.integer(forKey: udid)
-        numberingLabel.text = "\(numbering)"
+        numberingLabel.text = "\(numbering), count: \(personList.count)"
     }
+    
     func add(person: Person) {
         try! realm.write {
             realm.add(person)
         }
     }
-
+    
+    func delete(person: Person) {
+        try! realm.write {
+            realm.delete(person)
+        }
+    }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
         let genderList: [Gender] = [.woman, .none]
+        let personList = realm.objects(Person.self)
         let randomGender = genderList.randomElement() ?? .woman
         let newPerson = Person(name: "name_\(numbering)", age: numbering, gender: randomGender)
         add(person: newPerson)
         
         numbering += 1
         UserDefaults.standard.setValue(numbering, forKey: udid)
-        numberingLabel.text = "\(UserDefaults.standard.integer(forKey: udid))"
+        numberingLabel.text = "\(UserDefaults.standard.integer(forKey: udid)) / count: \(personList.count)"
+    }
+    
+    @IBAction func deleteButtonClicked(_ sender: Any) {
+        let personList = realm.objects(Person.self)
+        let person = personList[0]
+        delete(person: person)
+        numberingLabel.text = "\(UserDefaults.standard.integer(forKey: udid)) / count: \(personList.count)"
     }
 }
 
