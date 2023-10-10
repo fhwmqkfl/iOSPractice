2b70bfb39c3addad51a39cd4c29d59e95eb2ce33
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-10-10 16:25:18 +0900

    feat(FaceViewEX) : draw face with UIBezierPath

---
+++ FaceViewEX/FaceViewEX/FaceView.swift
@@ -0,0 +1,86 @@
+//
+//  FaceView.swift
+//  FaceViewEX
+//
+//  Created by coco on 2023/10/10.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    enum Eye {
+        case Left
+        case Right
+    }
+    
+    var scale: CGFloat = 0.90
+    let mouthCurvature: Double = 1.0 // 1 full smile, -1 full frown
+    private var skullRadious: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2
+    }
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    // 눈을 만들기위해 두개골의 반경과
+    private struct Ratios {
+        static let SkullRadiousToEyeOffset: CGFloat = 3
+        static let SkullRadiousToEyeRadius: CGFloat = 10
+        static let SkullRadiousToMouthWidth: CGFloat = 1
+        static let SkullRadiousToMouthHeight: CGFloat = 3
+        static let SkullRadiousToMouthOffset: CGFloat = 3
+    }
+    
+    override func draw(_ rect: CGRect) {
+        UIColor.blue.set()
+        pathForCircleCenteredAtPoint(midPoint: skullCenter, withRadius: skullRadious).stroke() // 여기까지한걸 그림
+        pathForEye(eye: .Left).stroke()
+        pathForEye(eye: .Right).stroke()
+        pathForMouth().stroke()
+    }
+    
+    private func pathForCircleCenteredAtPoint(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: midPoint, radius: radius, startAngle: 0.0, endAngle: 2*Double.pi, clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
+    
+    private func getEyeCenter(_ eye: Eye) -> CGPoint {
+        let eyeOffset = skullRadious / Ratios.SkullRadiousToEyeOffset
+        var eyeCenter = skullCenter
+        eyeCenter.y -= eyeOffset
+        switch eye {
+        case .Left: eyeCenter.x -= eyeOffset
+        case .Right: eyeCenter.x += eyeOffset
+        }
+        
+        return eyeCenter
+    }
+    
+    private func pathForEye(eye: Eye) -> UIBezierPath {
+        let eyeRadius = skullRadious / Ratios.SkullRadiousToEyeRadius
+        let eyeCenter = getEyeCenter(eye)
+        
+        return pathForCircleCenteredAtPoint(midPoint: eyeCenter, withRadius: eyeRadius)
+    }
+    
+    private func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadious / Ratios.SkullRadiousToMouthWidth
+        let mouthHeight = skullRadious / Ratios.SkullRadiousToMouthHeight
+        let mouthOffset = skullRadious / Ratios.SkullRadiousToMouthOffset
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth/2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
+        let cp1 = CGPoint(x: mouthRect.minX + mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        let cp2 = CGPoint(x: mouthRect.maxX - mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 5.0
+        
+        return path
+    }
+    
+}
---
+++ FaceViewEX/FaceViewEX/FaceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceViewEX
+//
+//  Created by coco on 2023/10/10.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
