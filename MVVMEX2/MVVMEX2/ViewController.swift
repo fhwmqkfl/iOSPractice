3336967ed7066ac0bc4f8d700c01c975849c4422
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-10-20 15:30:58 +0900

    Initial commit

---
+++ MVVMEX2/MVVMEX2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMEX2
+//
+//  Created by coco on 2023/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
