688ca8d6cad99f8dd3616cf5e86b7a90d749e40a
Yeoju Seok <yeoju.ac@gmail.com>
2024-02-26 21:08:06 +0900

    feat: add stackview

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabCount = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        Button("Tab Count : \(tabCount)") {
+//            tabCount += 1
+//        }
+        HStack{
+            VStackView()
+            VStackView()
+            VStackView()
         }
-        .padding()
+        .padding(10)
+        .background(.yellow)
+
+    }
+
+
+}
+
+struct VStackView: View {
+    var body: some View {
+        VStack{
+            Text("1!")
+                .fontWeight(.bold)
+                .font(.system(size: 60))
+            Text("2!")
+                .font(.system(size: 60))
+                .fontWeight(.bold)
+            Text("3!")
+                .font(.system(size: 60))
+                .fontWeight(.bold)
+        }
+        .background(.orange)
     }
 }
 
