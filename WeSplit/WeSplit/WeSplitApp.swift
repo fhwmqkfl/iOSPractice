aace5122f37361a78fb3b22a1001338f49eedf84
Yeoju Seok <yeoju.ac@gmail.com>
2024-02-26 13:42:51 +0900

    feat: first init

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by yeojuseok on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+// AppDelegate/SceneDelegate같은
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
