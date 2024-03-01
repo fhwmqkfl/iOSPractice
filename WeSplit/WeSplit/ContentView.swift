77c1e0528b42164b0361f2fe605c6b8836f36c36
Yeoju Seok <yeoju.ac@gmail.com>
2024-03-01 19:48:31 +0900

    Feat: add calculate price & modify keyboard setting

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,16 +11,32 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
 
     let tipPercentages = [10, 15, 20, 25, 0]
+
+    var totalPerson: Double {
+        // calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+
     var body: some View {
         NavigationStack {
             // 식사에 얼마를 내야하는 계산하는 폼
             Form {
                 // 가격입력 폼 & 키보드는 숫자만
                 // 사람이 몇명인지 선택하는화면
+                // 팁이 얼마인지 확인하는 화면
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
 
                     Picker("number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -29,12 +45,28 @@ struct ContentView: View {
                     }
                 }
 
+                Section("How much do you want to tip?") {
+                    Picker("tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
                 // 화면 보여주는 폼
                 Section {
                     Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("weSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
